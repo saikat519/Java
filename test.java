e827977c85a6caa4e68a4e34f9d93d9d8f13126c
saikat519 <saikat.mukhopadhyay17@gmail.com>
2020-10-07 15:28:16 +0530

    initial commit

---
+++ test.java
@@ -0,0 +1,15 @@
+package OOPS;
+import java.util.Arrays;
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int[] arr = {3,1,0,5,-2};
+			
+			Arrays.sort(arr);
+			for(int i=0;i<arr.length;i++) {
+				System.out.println(arr[i]);
+			}
+	}
+
+}
